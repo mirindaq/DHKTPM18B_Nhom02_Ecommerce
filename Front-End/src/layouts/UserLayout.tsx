633d7766a614c33687303a9323e588c3beacbfe5
Viet Hoang <gvhoang30112004@gmail.com>
2025-09-23 10:36:56 +0700

    fix

--- Front-End/src/layouts/UserLayout.tsx
+++ Front-End/src/layouts/UserLayout.tsx
@@ -1,7 +1,7 @@
 
 import Footer from "@/components/user/Footer/Footer";
 import Header from "@/components/user/Header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router";
 
 export default function UserLayout() {
   return (
