b3a5fbe937b66a246eb86717b5cd661ee5611d31
daocongdanh <daocongdanh47@gmail.com>
2025-11-15 13:48:41 +0700

    feat: add payos payment

--- Back-End/src/main/java/iuh/fit/ecommerce/services/PaymentService.java
+++ Back-End/src/main/java/iuh/fit/ecommerce/services/PaymentService.java
@@ -10,4 +10,7 @@ import java.util.List;
 public interface PaymentService {
     String createPaymentUrl(Voucher voucher, Order order, List<Long> cartItemIds, HttpServletRequest request);
     void handlePaymentCallBack(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    String createPayOsPaymentUrl(Voucher voucher, Order order, List<Long> cartItemIds);
+    void handlePayOsSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    void handlePayOsCancel(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
